eb6a6fe4dee7443671e0b4f4e27ddae8e1ff1baf
kirito-o <36725291+kirito-o@users.noreply.github.com>
2022-05-21 12:18:11 +0800

    Update Bili_Auto_Regions.js

--- Bili_Auto_Regions.js
+++ Bili_Auto_Regions.js
@@ -12,10 +12,10 @@ let $ = nobyda();
 let run = EnvInfo();
 
 async function SwitchRegion(play) {
-	const Group = $.read('BiliArea_Policy') || '港台番剧'; //Your blibli policy group name.
+	const Group = $.read('BiliArea_Policy') || '香港'; //Your blibli policy group name.
 	const CN = $.read('BiliArea_CN') || 'DIRECT'; //Your China sub-policy name.
-	const TW = $.read('BiliArea_TW') || '台湾节点'; //Your Taiwan sub-policy name.
-	const HK = $.read('BiliArea_HK') || '香港节点'; //Your HongKong sub-policy name.
+	const TW = $.read('BiliArea_TW') || '台湾'; //Your Taiwan sub-policy name.
+	const HK = $.read('BiliArea_HK') || '香港'; //Your HongKong sub-policy name.
 	const current = await $.getPolicy(Group);
 	const area = (() => {
 		if (/\u50c5[\u4e00-\u9fa5]+\u6e2f|%20%E6%B8%AF&/.test(play)) {
